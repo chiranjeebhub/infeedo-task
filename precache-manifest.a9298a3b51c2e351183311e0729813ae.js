c7635cb909909e74bc081a07dac647cea1e650ab
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-05-12 13:34:31 +0530

    Updates

---
+++ precache-manifest.a9298a3b51c2e351183311e0729813ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87640e1587d0fa60fdd95478d0c53bea",
+    "url": "/infeedo-task/index.html"
+  },
+  {
+    "revision": "f9531e2bf9819106738d",
+    "url": "/infeedo-task/static/css/main.67a19627.chunk.css"
+  },
+  {
+    "revision": "d98de92e52e2fa1612be",
+    "url": "/infeedo-task/static/js/2.7ca4214c.chunk.js"
+  },
+  {
+    "revision": "f9531e2bf9819106738d",
+    "url": "/infeedo-task/static/js/main.75eb21d5.chunk.js"
+  },
+  {
+    "revision": "0985291076502e432ae1",
+    "url": "/infeedo-task/static/js/runtime~main.40678114.js"
+  },
+  {
+    "revision": "e7eab5562a9a1c9c58b9a8be10ed2e00",
+    "url": "/infeedo-task/static/media/hamburger.e7eab556.png"
+  }
+]);
